32d533ee25e51c504878d37b51a01df439ba731c
WK-Done <WasiDev@hotmail.com>
2023-11-19 10:16:12 +0100

    Add project files.

---
+++ BlazorDemo.Tests/BunitTestContext.cs
@@ -0,0 +1,18 @@
+namespace BlazorDemo.Tests
+{
+  using Bunit;
+  using NUnit.Framework;
+
+  /// <summary>
+  /// Test context wrapper for bUnit.
+  /// Read more about using <see cref="BunitTestContext"/> <seealso href="https://bunit.dev/docs/getting-started/writing-tests.html#remove-boilerplate-code-from-tests">here</seealso>.
+  /// </summary>
+  public abstract class BunitTestContext : TestContextWrapper
+  {
+    [SetUp]
+    public void Setup() => TestContext = new Bunit.TestContext();
+
+    [TearDown]
+    public void TearDown() => TestContext?.Dispose();
+  }
+}
---
+++ BlazorTests/Model/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDemo.Model
+{
+  public class Person
+  {
+    public string Firstname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+  }
+}
